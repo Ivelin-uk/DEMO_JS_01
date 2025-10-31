7ef6bdd60516393497f95b3de327b64f886e6d2e
Ivelin-uk <ivoagaininwow@gmail.com>
2025-10-31 21:54:31 +0200

    first commit

---
+++ loops/demo_loops_01.js
@@ -0,0 +1,13 @@
+// console.log('before');
+// for(let i = 5; i <= 10; i++) {
+//     console.log(i)
+// }
+// console.log('after');
+
+// let firstName = "Ivan"
+// let lastName = "Ivanov"
+// //let fillName = "My name is " + firstName + " " + lastName;
+// let fillName = `My name is ${firstName} ${lastName}`
+
+// console.log(fillName)
+
